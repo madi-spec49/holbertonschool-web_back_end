4cf0a6b87f14b2308599fa8b4cfeaeb3717cf23c
madi-spec49 <madi.anli.madi@gmail.com>
2026-06-15 16:51:04 +0200

    task3

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Task creation module"""
+
+import asyncio
+from typing import Any
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Create and return an asyncio.Task for wait_random
+
+    Args:
+        max_delay (int): Maximum delay in seconds
+
+    Returns:
+        asyncio.Task: Task object that will execute wait_random
+    """
+    return asyncio.create_task(wait_random(max_delay))
