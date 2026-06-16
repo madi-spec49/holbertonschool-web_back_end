9929d7bcc112b4d17c160b2b8b78ee7c8ccfb1e9
madi-spec49 <madi.anli.madi@gmail.com>
2026-06-16 14:26:25 +0200

    async generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import asyncio
 import random
 
@@ -7,3 +9,6 @@ async def async_generator():
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
+
+if __name__ == "__main__":
+    asyncio.run(main())
